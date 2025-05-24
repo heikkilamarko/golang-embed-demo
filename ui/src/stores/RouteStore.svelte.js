44f906e92a04268ff86fa39a8180c3f74d3a43b2
Marko Heikkilä <heikkila_marko@hotmail.com>
2025-05-24 18:57:50 +0300

    Cleanup.

--- ui/src/stores/RouteStore.svelte.js
+++ ui/src/stores/RouteStore.svelte.js
@@ -1,12 +1,17 @@
 import UniversalRouter from 'universal-router';
+import generateUrls from 'universal-router/generateUrls';
 
 export class RouteStore {
 	routes = $state([]);
 	route = $state();
 	router;
+	urls;
 
 	start() {
 		this.router = new UniversalRouter(this.routes);
+		this.urls = generateUrls(this.router, {
+			stringifyQueryParams: (params) => new URLSearchParams(params).toString()
+		});
 
 		document.addEventListener('click', (e) => {
 			const link = e.target.closest('a[data-link]');
@@ -22,11 +27,25 @@ export class RouteStore {
 		this.render(window.location.href);
 	}
 
+	navigate(routeName, routeParams) {
+		const href = this.urls(routeName, routeParams);
+		history.pushState(null, '', href);
+		this.render(href);
+	}
+
+	navigateBack() {
+		history.back();
+	}
+
 	async render(href) {
 		console.debug(href);
 		try {
 			const url = new URL(href, window.location.origin);
-			const route = await this.router.resolve({ pathname: url.pathname });
+			const queryParams = Object.fromEntries(new URLSearchParams(url.search));
+			const route = await this.router.resolve({
+				pathname: url.pathname,
+				queryParams
+			});
 			if (route.redirect) {
 				window.location = route.redirect;
 				return;
